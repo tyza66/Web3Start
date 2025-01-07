ac87e1d5d47f8e1b9f66e461a99fb375422ed48c
洮羱芝闇 <shun_@outlook.com>
2025-01-07 22:59:58 +0800

    Update main.py

--- 代码/python/c01/main.py
+++ 代码/python/c01/main.py
@@ -4,6 +4,7 @@ import json
 from time import time
 from urllib.parse import urlparse
 
+import requests
 from flask import Flask, jsonify, request
 
 
@@ -38,9 +39,9 @@ class BolckChain:
     # 添加一个交易
     def new_transaction(self, sender, recipient, amount) -> int:
         self.current_transactions.append({
-            'sender': sender, # 发送者
-            'recipient': recipient, # 接收者
-            'amount': amount #金额
+            'sender': sender,  # 发送者
+            'recipient': recipient,  # 接收者
+            'amount': amount  # 金额
         })
         return self.last_block['index'] + 1
 
@@ -49,14 +50,14 @@ class BolckChain:
         # 将区块转换为json字符串，然后转换为字节，然后进行sha256加密 最后返回加密后的哈希值
         return hashlib.sha256(json.dumps(block, sort_keys=True).encode()).hexdigest()
 
-    @property # 将函数定义为属性
+    @property  # 将函数定义为属性
     def last_block(self):
         return self.chain[-1]  # 返回最后一个区块
 
     # 模拟工作量证明 真实的应该是前一个区块的哈希值、当前区块的哈希值、一个随机数一起哈希验证
     def proof_of_work(self, last_proof):
         proof = 0
-        while self.valid_proof(last_proof, proof) is False: # 最后一个节点的工作量证明和当前的工作量证明进行加密，然后判断加密后的哈希值是否满足条件
+        while self.valid_proof(last_proof, proof) is False:  # 最后一个节点的工作量证明和当前的工作量证明进行加密，然后判断加密后的哈希值是否满足条件
             proof += 1
         return proof
 
@@ -66,6 +67,39 @@ class BolckChain:
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
+
+    # 验证区块链是否有效
+    def valid_chain(self, chain):
+        last_block = chain[0]
+        current_index = 1
+        while current_index < len(chain):
+            block = chain[current_index]
+            if block['previous_hash'] != self.hash(last_block):  # 判断当前区块的哈希值是否等于上一个区块的哈希值
+                return False
+            if not self.valid_proof(last_block['proof'], block['proof']):  # 判断工作量证明是否有效
+                return False
+            last_block = block
+            current_index += 1
+        return True
+
+    # 解决冲突
+    def resolve_conflicts(self):
+        neighbours = self.nodes  # 获取所有的节点 目的是找到最长的区块链
+        new_chain = None
+        max_length = len(self.chain)
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+                if length > max_length and self.valid_chain(chain):  # 如果长度大于当前的区块链长度并且区块链是有效的
+                    max_length = length
+                    new_chain = chain
+        if new_chain:
+            self.chain = new_chain
+            return True
+        return False
+
 
 def init_flask():
     app = Flask(__name__)
@@ -80,14 +114,14 @@ def init_flask():
     @app.route('/mine', methods=['GET'])
     def mine():
         last_block = blockchain.last_block
-        last_proof = last_block['proof']    # 获取最后一个区块的工作量证明模拟用
-        proof = blockchain.proof_of_work(last_proof) # 模拟计算当前节点工作量证明(浪费自然资源)
-        blockchain.new_transaction(     # 给自己的奖励交易
-            sender="0", #奖励不需要发送者 相当于新发行的货币
-            recipient=node_identifier, # 接收者是自己
-            amount=1    # 奖励金额
+        last_proof = last_block['proof']  # 获取最后一个区块的工作量证明模拟用
+        proof = blockchain.proof_of_work(last_proof)  # 模拟计算当前节点工作量证明(浪费自然资源)
+        blockchain.new_transaction(  # 给自己的奖励交易
+            sender="0",  # 奖励不需要发送者 相当于新发行的货币
+            recipient=node_identifier,  # 接收者是自己
+            amount=1  # 奖励金额
         )
-        block = blockchain.new_block(proof) # 给最新的交易同步进区块链 之后返回当前添加的区块信息
+        block = blockchain.new_block(proof)  # 给最新的交易同步进区块链 之后返回当前添加的区块信息
         response = {
             'message': "New Block Forged",
             'index': block['index'],
