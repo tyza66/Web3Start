37f5a4e6287365e207081034d8d9741f60264078
洮羱芝闇 <shun_@outlook.com>
2025-01-08 10:37:51 +0800

    更新

--- 代码/solidity/c02/solidity2.sol
+++ 代码/solidity/c02/solidity2.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.8.28; 
 
-
+// 这个是合约1用来import用的
 contract Solidity2{
     int i;
 	// Add a function or other valid definition here
---
+++ 代码/solidity/c02/solidity3.sol
@@ -0,0 +1,3 @@
+pragma solidity 0.8.28; 
+
+// solidity是静态类型语言 所以需要声明变量的类型
