4d19c150248aaa67d2eaa6e95eb0019f43e351cf
洮羱芝闇 <shun_@outlook.com>
2025-01-10 11:52:30 +0800

    点一下出来三个订单？

--- 代码/DAPP/C03/03.宠物领养DAPP/pet-shop/src/js/app.js
+++ 代码/DAPP/C03/03.宠物领养DAPP/pet-shop/src/js/app.js
@@ -28,7 +28,7 @@ App = {
     if (typeof web3 != 'undefined') {
       App.web3Provider = web3.currentProvider
     } else {
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      App.web3Provider = new Web3.providers.HttpProvider('http://127.0.0.1:7545');
     }
     web3 = new Web3(App.web3Provider); // 之后给web3设置provider
     // 创建的这个web3对象，就是我们在前端调用的web3对象，这个对象是一个全局对象，我们可以在任何地方调用它
@@ -61,7 +61,7 @@ App = {
       return instance.getAdopters.call(); // 调用合约的getAdopters方法
     }).then(function (adopters) {
       for (i = 0; i < adopters.length; i++) {
-        if (adopters[i] !== '0x00000000000000000000000000000000') {
+        if (adopters[i] != "0x0000000000000000000000000000000000000000") {
           $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
         }
       }
@@ -70,10 +70,21 @@ App = {
     });
   },
 
-  handleAdopt: function (event) {
+  //如果你将加密货币发送到一个不存在的地址上，交易仍然会被记录在区块链上，并且会扣除你的资金。在区块链上，一旦交易被广播并确认，它就无法撤销。如果目标地址确实不存在，这些币将无法被使用或找回
+
+  handleAdopt: async function (event) {
     event.preventDefault();
 
     var petId = parseInt($(event.target).data('id'));
+
+    // 新版里面需要用户授权
+    try {
+      // 请求用户授权
+      await App.web3Provider.enable();
+    } catch (error) {
+      // 用户不授权时
+      console.error("User denied account access")
+    }
 
     web3.eth.getAccounts(function (error, accounts) {
       if (error) {
@@ -84,8 +95,8 @@ App = {
 
       // 每次调用 deployed() 方法时，都会返回已经部署的合约实例。这意味着你获取到的合约实例是相同的，并且引用的是同一个智能合约部署地址
       App.contracts.Adoption.deployed().then(function (instance) {
-      // 方法名调用用于调用会改变区块链状态的方法，例如更新合约中的数据。这些方法会消耗 gas 并产生交易 这种调用方式需要将事务提交到区块链，因此需要指定 from 地址等参数
-        return instance.adopt(petId, { from: account });
+        // 方法名调用用于调用会改变区块链状态的方法，例如更新合约中的数据。这些方法会消耗 gas 并产生交易 这种调用方式需要将事务提交到区块链，因此需要指定 from 地址等参数
+        return instance.adopt(petId, { from: account }); // account是调用者的地址
       }).then(function (result) {
         console.log(result);
         return App.markAdopted(); // 更新当前的领养状态
