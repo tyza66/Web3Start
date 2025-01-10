777589d0d653b60d3ebbc747c0496c37a89b5cd7
洮羱芝闇 <shun_@outlook.com>
2025-01-10 08:13:30 +0800

    更改测试网络

--- 代码/DAPP/C03/03.宠物领养DAPP/pet-shop/test/TestAdoption.sol
+++ 代码/DAPP/C03/03.宠物领养DAPP/pet-shop/test/TestAdoption.sol
@@ -20,4 +20,14 @@ contract TestAdoption {
         address adopter = adoption.adopters(8);
         Assert.equal(adopter, expected, "Owner of pet ID 8 should be recorded.");
     }
+
+    // 测试获取所有领养者
+    function testGetAdopterAddressByPetIdInArray() public {
+        address expected = address(this);
+        address[16] memory adopters = adoption.getAdopters();
+        Assert.equal(adopters[8], expected, "Owner of pet ID 8 should be recorded.");
+    }
 }
+
+// 想看到产生区块效果就不能走默认的test网络
+// 使用truffle test --network development
